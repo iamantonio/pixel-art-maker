c98a9921f3aaabda31d24131aa4cba19e7c3af74
Antonio Vargas <40072001+iamantonio@users.noreply.github.com>
2020-01-13 21:26:47 -0600

    created function to get color value, and to change background color when user clicks. cleaned up and added comments.

--- designs.js
+++ designs.js
@@ -1,20 +1,38 @@
+/*jshint esversion: 6 */
+/*
+*  Pixel Art Maker Project
+*  By: Antonio Vargas
+* */
+// Created variables that store the value of what the
+// user inputted for Height and Width of the grid.
 let gridHeight = 0;
 let gridWidth = 0;
 
+// Created variables to target elements by ID
 const tableNode = document.getElementById('pixelCanvas');
+const colorPicker = document.getElementById('colorPicker');
 
-function myFunc() {
-    console.log(`Grid Height: ${gridHeight}\nGrid Width: ${gridWidth}`)
-}
+// Created function to debug code in the browser console.
+// function myFunc() {
+//     console.log(`Grid Height: ${gridHeight}\nGrid Width: ${gridWidth}`);
+// }
+
 // Created this function to clear the grid canvas when user inputs new values.
+// Or if the user wants to clear the colors on the existing grid.
 function clearGrid() {
     while(tableNode.hasChildNodes()) {
         tableNode.removeChild(tableNode.firstChild);
-    };
+    }
 
 }
 
-// Select color input
+// Function created that selects color input
+function getColor() {
+    // Get the value of the color
+    const colorSelected = colorPicker.value;
+    return colorSelected;
+}
+
 
 // Select size input
 const sizePicker = document.querySelector('#sizePicker');
@@ -22,10 +40,12 @@ sizePicker.addEventListener('submit', function (e) {
     e.preventDefault();
     gridHeight = document.getElementById('inputHeight').value;
     gridWidth = document.getElementById('inputWidth').value;
+    // Conditional to prevent site from crashing if user puts in a value
+    // that is too high.
     if(gridWidth >= 101 || gridHeight >= 101) {
-        alert('Height and\/or Width Value Too High!')
+        alert('Height and\/or Width Value Too High!');
     } else {
-        myFunc();
+        //myFunc();
         clearGrid();
         makeGrid();
     }
@@ -36,17 +56,18 @@ sizePicker.addEventListener('submit', function (e) {
 
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-    let i = 0;
+    let i = 0; // Iterator set for the loop below.
+    // A while loop that creates the table based on the user input for
+    // the grid size.
     while(i < gridHeight){
-        //console.log('element added');
         const tr = document.createElement('tr');
-        tr.setAttribute('id', `row-${i}`)
+        tr.setAttribute('id', `row-${i}`);
         tableNode.appendChild(tr);
-        let x = 0;
+        let x = 0; // Iterator for the nested loop to create the columns.
         while(x < gridWidth) {
             const td = document.createElement('td');
+            td.setAttribute('id', `col-${x}`);
             document.getElementById(`row-${i}`).appendChild(td);
             x ++;
         }
@@ -54,3 +75,13 @@ function makeGrid() {
     }
 
 }
+
+// Function to go change the CSS Background Color of a target element.
+function colorBackGround(e) {
+    const target = e.target;
+    target.style.backgroundColor = getColor();
+}
+
+// Added event listener to change background color of cell in grid when
+// the user presses the button on the mouse.
+tableNode.addEventListener('mousedown', colorBackGround);
